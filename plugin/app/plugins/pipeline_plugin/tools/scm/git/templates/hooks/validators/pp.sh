532a148653883edcb71e34e5c96cf6d5955e5ad6
carlosg <carlosg@tid.es>
2016-01-18 12:41:24 +0100

    future parser

--- plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/pp.sh
+++ plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/pp.sh
@@ -2,5 +2,5 @@
 # Validate puppet
 function validate(){
   which puppet 2>/dev/null >/dev/null || return 126
-  puppet parser validate $file_name 2>/dev/stdout
+  puppet parser --parser=future validate $file_name 2>/dev/stdout
 }
