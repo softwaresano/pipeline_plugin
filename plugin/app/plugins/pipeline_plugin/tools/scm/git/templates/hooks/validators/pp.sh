2054b4b06652dc3c06ecc8161e90c21b222b07c3
carlosg <carlosg@tid.es>
2016-03-31 16:14:08 +0200

    Added puppet-lint

--- plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/pp.sh
+++ plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/pp.sh
@@ -3,4 +3,6 @@
 function validate(){
   which puppet 2>/dev/null >/dev/null || return 126
   puppet parser --parser=future validate $file_name 2>/dev/stdout
+  which puppet-lint 2>/dev/null >/dev/null || return 0
+  puppet-lint --no-puppet_url_without_modules-check --no-80chars-check --fix --with-filename "$file_name" 2>/dev/stdout
 }
