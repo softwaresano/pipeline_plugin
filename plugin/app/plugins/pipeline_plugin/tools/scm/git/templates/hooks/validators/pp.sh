ab109db35430e76c9c345c007a209bb98c22541a
carlosg <carlosg@tid.es>
2016-03-31 18:28:59 +0200

    Disable autoloader layout

--- plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/pp.sh
+++ plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/pp.sh
@@ -2,7 +2,7 @@
 # Validate puppet
 function validate(){
   which puppet 2>/dev/null >/dev/null || return 126
-  puppet parser --parser=future validate $file_name 2>/dev/stdout
+  puppet parser --parser=future validate $file_name 2>/dev/stdout || return 1
   which puppet-lint 2>/dev/null >/dev/null || return 0
   puppet-lint --no-autoloader_layout-check --no-puppet_url_without_modules-check --no-80chars-check --fix --with-filename "$file_name" 2>/dev/stdout
 }
