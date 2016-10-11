c0c95c244ff9c942b2c63e9c9796ae5df67a363f
carlosg <carlosg@tid.es>
2016-10-11 16:25:38 +0200

    release notes.txt

--- plugin/app/plugins/pipeline_plugin/profiles/publish/rpm/post-publish/publish-last-merge.sh
+++ plugin/app/plugins/pipeline_plugin/profiles/publish/rpm/post-publish/publish-last-merge.sh
@@ -4,7 +4,7 @@ item=$(git log -1|egrep  "Merge pull request #[0-9]+ from"|sed s:"    Merge pull
 [ -z $DP_HOME ] && export DP_HOME=$(python -c 'import os,sys;print os.path.realpath(sys.argv[1])' $0/..)
 source $DP_HOME/profiles/publish/rpm/dp_publish.sh
 version=$(dp_version.sh)
-filename="$(dirname $(get_repo_dir))/release-notes-$version"
+filename="$(dirname $(get_repo_dir))/release-notes-$version.txt"
 repo=$(git config --get remote.origin.url|grep -Po ":.*/.*\.git"|cut -d ':' -f2-)
 date=$(git log -1|grep ^Date|cut -d ' ' -f4-)
 line="${repo}\t${date}\t${item}"
