5f713b57b0f2e3be2c0087b6f9979f4526ce1f42
carlosg <carlosg@tid.es>
2016-10-11 16:19:59 +0200

    Publish info with last merge

---
+++ plugin/app/plugins/pipeline_plugin/profiles/publish/rpm/post-publish/publish-last-merge.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+item=$(git log -1|egrep  "Merge pull request #[0-9]+ from"|sed s:"    Merge pull request.*from":"":g|awk '{print $1}'|cut -d'/' -f2-)
+[[ "$item" == "" ]] && exit 0
+[ -z $DP_HOME ] && export DP_HOME=$(python -c 'import os,sys;print os.path.realpath(sys.argv[1])' $0/..)
+source $DP_HOME/profiles/publish/rpm/dp_publish.sh
+version=$(dp_version.sh)
+filename="$(dirname $(get_repo_dir))/release-notes-$version"
+repo=$(git config --get remote.origin.url|grep -Po ":.*/.*\.git"|cut -d ':' -f2-)
+date=$(git log -1|grep ^Date|cut -d ' ' -f4-)
+line="${repo}\t${date}\t${item}"
+>$filename
+[[ "$(grep \"'$line'\" $filename)" == '' ]] && echo -e $line>>$filename
+exit 0
