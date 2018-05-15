c986eb15d075009413fd33bf3705aa950ee38dcb
carlosg <carlosenrique.gomezgomez@telefonica.com>
2018-05-15 15:17:20 +0200

    repoview fixed

--- plugin/app/plugins/pipeline_plugin/profiles/package/redhat/createrepo.sh
+++ plugin/app/plugins/pipeline_plugin/profiles/package/redhat/createrepo.sh
@@ -4,5 +4,5 @@ function createrepo(){
     [[ "$1" == "" ]] && _log "[ERROR] directory parameter is mandatory" && exit 1
     eval "$REMOTE_COMMAND mkdir -p $directory" && \
        eval "$REMOTE_COMMAND /usr/bin/createrepo -s sha -d --update $directory" && \
-       eval "$REMOTE_COMMAND /usr/bin/repoview $directory"
+       eval "$REMOTE_COMMAND 'rm -rf $directory/repoview;/usr/bin/repoview $directory'"
 }
