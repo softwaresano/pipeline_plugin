ef18d3886cf032094f1d555cb15c48669f46761c
carlosg <carlosenrique.gomezgomez@telefonica.com>
2018-05-30 19:23:26 +0200

    fix

--- plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/bash.sh
+++ plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/bash.sh
@@ -2,5 +2,6 @@
 # Validate bash
 function validate(){
   source $validator_dir/non_ascii.sh
-  is_ascii && bash -n $file_name 2>/dev/stdout && shellcheck $file_name 2>/dev/stdout
+  echo bash
+  is_ascii && bash -n $file_name 2>/dev/stdout && shellcheck -s bash $file_name 2>/dev/stdout
 }
