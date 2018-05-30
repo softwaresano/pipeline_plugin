bc81503e3f1a17231f07296989627b8ed49df55b
mff245 <e.deliverycdn.tid@telefonica.com>
2018-05-30 13:15:49 +0200

    Fix

--- plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/bash.sh
+++ plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/bash.sh
@@ -2,6 +2,5 @@
 # Validate bash
 function validate(){
   source $validator_dir/non_ascii.sh
-  is_ascii && bash -n $file_name 2>/dev/stdout
-  shellcheck $file_name 2>/dev/stdout
+  is_ascii && bash -n $file_name 2>/dev/stdout && shellcheck $file_name 2>/dev/stdout
 }
--- plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/sh.sh
+++ plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/sh.sh
@@ -2,6 +2,5 @@
 # Validate sh
 function validate(){
   source $validator_dir/non_ascii.sh
-  is_ascii && sh -n $file_name  2>/dev/stdout
-  shellcheck $file_name 2>/dev/stdout
+  is_ascii && sh -n $file_name  2>/dev/stdout && shellcheck $file_name 2>/dev/stdout
 }
