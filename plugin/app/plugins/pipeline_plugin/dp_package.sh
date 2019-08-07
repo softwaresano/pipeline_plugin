a13acc96b9498512f22500aa94b5f84cbbcad898
no_user <no_user@no_mail.com>
2019-08-07 13:06:01 +0200

    python2 explicit

--- plugin/app/plugins/pipeline_plugin/dp_package.sh
+++ plugin/app/plugins/pipeline_plugin/dp_package.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 [ -z $DP_HOME ] && export DP_HOME=$(python2 -c 'import os,sys;print os.path.realpath(sys.argv[1])' $(which dp_package.sh)/..)
-### HELP section
+###HELP section
 dp_help_message='It creates a package of the project
 
 Usage: dp_package.sh'
