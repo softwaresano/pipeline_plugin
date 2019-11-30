d75c6bf357a21d02ae1ce77443f199642d648979
carlosg <carlosenrique.gomezgomez@telefonica.com>
2019-11-30 07:30:59 +0100

    fix

--- plugin/app/plugins/pipeline_plugin/profiles/publish/rpm/post-publish/publish-last-merge.sh
+++ plugin/app/plugins/pipeline_plugin/profiles/publish/rpm/post-publish/publish-last-merge.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 item=$(git log -1|egrep  "Merge pull request #[0-9]+ from"|sed s:"    Merge pull request.*from ":"":g|awk '{print $1}'|cut -d'/' -f2-)
 [[ "$item" == "" ]] && exit 0
-[ -z $DP_HOME ] && export DP_HOME=$(python2 -c 'import os,sys;print os.path.realpath(sys.argv[1])' $0/..)
+[ -z $DP_HOME ] && export DP_HOME=$(python2 -c 'import os,sys;print os.path.realpath(sys.argv[1])' $0/../../../../../)
 source $DP_HOME/profiles/publish/rpm/dp_publish.sh
 version=$(dp_version.sh)
 filename="$(dirname $(get_repo_dir))/release-notes-$version.txt"
