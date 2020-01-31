56cfaf3dfa12d7c8d7e7f3fe1edf58534fa262cc
carlosg <carlosenrique.gomezgomez@telefonica.com>
2020-01-31 10:03:24 +0100

    depreated parameter in puppet parser

--- plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/pp.sh
+++ plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/pp.sh
@@ -3,7 +3,7 @@
 function validate(){
   source $validator_dir/non_ascii.sh
   which puppet 2>/dev/null >/dev/null || return 126
-  puppet parser --parser=future validate $file_name 2>/dev/stdout || return 1
+  puppet parser validate $file_name 2>/dev/stdout || return 1
   which puppet-lint 2>/dev/null >/dev/null || return 0
   puppet-lint --no-autoloader_layout-check --no-puppet_url_without_modules-check --no-80chars-check --fix --with-filename "$file_name" 2>/dev/stdout
 }
