0c205def5b729b88857d3e860de0c38af35f6cf4
carlosg <carlosenrique.gomezgomez@telefonica.com>
2020-10-22 10:39:03 +0200

    fail if certifcate is oudated

--- plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/pem.sh
+++ plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/pem.sh
@@ -2,5 +2,7 @@
 # Validate pem
 function validate(){
   which openssl 2>/dev/null >/dev/null || return 126
-  openssl x509 -in "${file_name}" -noout -text 2>/dev/stdout
+  openssl x509 -checkend 1 -in "${file_name}" -noout 2>/dev/stdout
 }
+
+ -checkend 1
