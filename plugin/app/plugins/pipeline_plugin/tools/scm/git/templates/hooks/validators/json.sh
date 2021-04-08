e655870358eab44eac96854ec5cbd382089c37bf
carlosg <carlosenrique.gomezgomez@telefonica.com>
2021-04-08 22:21:01 +0200

    json_verify

--- plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/json.sh
+++ plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/json.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # Validate json
 function validate(){
-  which jsonlint 2>/dev/null >/dev/null || return 126
-  jsonlint -v $file_name 2>/dev/stdout|grep -v ": ok$"
-  return ${PIPESTATUS[0]}
+  which json_verify 2>/dev/null >/dev/null || return 0
+  json_verify < "${file_name}"
 }
