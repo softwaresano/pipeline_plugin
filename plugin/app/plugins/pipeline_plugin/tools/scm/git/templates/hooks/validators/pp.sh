f2a59d9402583fa01843b96fcb0c4edb3f570cd8
carlosg <carlosenrique.gomezgomez@telefonica.com>
2021-04-19 12:04:18 +0200

    puppet hook fixed

--- plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/pp.sh
+++ plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/pp.sh
@@ -2,8 +2,8 @@
 # Validate puppet
 function validate(){
   source $validator_dir/non_ascii.sh
-  which puppet 2>/dev/null >/dev/null || return 126
-  puppet parser validate $file_name 2>/dev/stdout || return 1
+  [[ -f /opt/puppetlabs/bin/puppet ]] || return 126
+  /opt/puppetlabs/bin/puppet parser validate $file_name 2>/dev/stdout || return 1
   which puppet-lint 2>/dev/null >/dev/null || return 0
   puppet-lint --no-autoloader_layout-check --no-puppet_url_without_modules-check --no-80chars-check --fix --with-filename "$file_name" 2>/dev/stdout
 }
