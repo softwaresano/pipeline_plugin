dae9e34e72301c9bd3d476cc8e008466939e2bc8
carlosg <carlosenrique.gomezgomez@telefonica.com>
2021-06-08 18:43:58 +0200

    shfmt hook

--- plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/sh.sh
+++ plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/sh.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # Validate sh
 function validate(){
-  sh -n $file_name  2>/dev/stdout && shellcheck -x $file_name 2>/dev/stdout
+  sh -n "$file_name" 2>/dev/stdout && \
+    shellcheck -x "$file_name" 2>/dev/stdout && \
+    shfmt -d -s -i 2 "$file_name"
 }
