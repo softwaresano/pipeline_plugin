80b11a62b8e212aca7ceb449f47d6ef45ae1259a
carlosg <carlosenrique.gomezgomez@telefonica.com>
2021-06-11 06:54:18 +0200

    shfmt error message

--- plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/bash.sh
+++ plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/bash.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # Validate bash
-function validate(){
-  bash -n "$file_name" 2>/dev/stdout && \
-    shellcheck -x -s bash "$file_name" 2>/dev/stdout && \
-    shfmt -d -s -i 2 "$file_name"
+function validate() {
+  bash -n "$file_name" 2>/dev/stdout || return 1
+  if ! shfmt -d -s -i 2 "$file_name"; then
+    dp_log.sh "[ERROR] shfmt. Run shfmt -i 2 -w -s $file_name to fix it"
+    return 1
+  fi
+  shellcheck -x -s bash "$file_name" 2>/dev/stdout
 }
