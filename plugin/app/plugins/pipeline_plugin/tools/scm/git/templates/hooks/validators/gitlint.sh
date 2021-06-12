d4479be1358a5f33c92c374e2722de625d9756d2
carlosg <carlosenrique.gomezgomez@telefonica.com>
2021-06-12 16:59:12 +0200

    Print gitlint error

--- plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/gitlint.sh
+++ plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/gitlint.sh
@@ -2,15 +2,13 @@ function validate() {
   local gitlint_temp
   local commit_message
   commit_message=$1
-  gitlint_temp=$(mktemp -u /tmp/gitlint.XXXX)
-  echo "$commit_message"| PYTHONUSERBASE=/opt/p2pcdn/var/lib/gitlint/ \
+  gitlint_temp=$(echo "$commit_message"| PYTHONUSERBASE=/opt/p2pcdn/var/lib/gitlint/ \
     /opt/p2pcdn/var/lib/gitlint/bin/gitlint \
-    --config  "${CDN_BUILD_LIB}"/linters/gitlint.ini &> "${gitlint_temp}"
+    --config  "${CDN_BUILD_LIB}"/linters/gitlint.ini &>/dev/stdout)
   retval_gitlint=$?
   if [[ "$retval_gitlint" != 0 ]]; then
     dp_log.sh "[ERROR] [KO] [gitlint] Bad commit message"
-    cat "${gitlint_temp}"
+    echo "${gitlint_temp}"
   fi
-  rm -f "${gitlint_temp}"
   return "${retval_gitlint}"
 }
