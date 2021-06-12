5c56244f52d8c2cba9601ec1e6f8ab6f328a0f09
carlosg <carlosenrique.gomezgomez@telefonica.com>
2021-06-12 17:09:33 +0200

    shebang

--- plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/gitlint.sh
+++ plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/gitlint.sh
@@ -1,12 +1,13 @@
+#!/bin/bash
 function validate() {
   local gitlint_temp
   local commit_message
   commit_message=$1
-  gitlint_temp=$(echo "$commit_message"| PYTHONUSERBASE=/opt/p2pcdn/var/lib/gitlint/ \
+  gitlint_temp=$(echo "$commit_message" | PYTHONUSERBASE=/opt/p2pcdn/var/lib/gitlint/ \
     /opt/p2pcdn/var/lib/gitlint/bin/gitlint \
-    --config  "${CDN_BUILD_LIB}"/linters/gitlint.ini &>/dev/stdout)
+    --config "${CDN_BUILD_LIB}"/linters/gitlint.ini &>/dev/stdout)
   retval_gitlint=$?
-  if [[ "$retval_gitlint" != 0 ]]; then
+  if [[ $retval_gitlint != 0 ]]; then
     dp_log.sh "[ERROR] [KO] [gitlint] Bad commit message"
     echo "${gitlint_temp}"
   fi
