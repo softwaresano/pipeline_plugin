805351f23b272c8c25ed1cf30f7e64c5f03cc53b
pag-telf <pablo.aceredagarcia.practicas@telefonica.com>
2021-08-04 07:19:36 +0200

    PTWOPCDN-19774 Tests for py validator

---
+++ tests/shell/validators/py_tests.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source "plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/py.sh"
+
+PYTHON_OK="tests/shell/validators/fixtures/python_ok.py"
+PYTHON_KO="tests/shell/validators/fixtures/python_failure.py"
+
+function test_py_ok() {
+  file_name="$PYTHON_OK" validate
+  ${_ASSERT_TRUE_} $?
+}
+
+function test_py_ko() {
+  file_name="$PYTHON_KO" validate
+  ${_ASSERT_FALSE_} $?
+}
+
+source "/opt/p2pcdn/var/lib/shUnit2/shunit2"
