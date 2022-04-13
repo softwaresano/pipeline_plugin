832d8015dcf4ba03d079669c49df8eb9f84c9936
carlosg <carlosenrique.gomezgomez@telefonica.com>
2022-04-13 07:43:56 +0200

    Rubocop and test makefile hook

--- plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/Makefile.sh
+++ plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/Makefile.sh
@@ -2,4 +2,9 @@
 # Validate makefile
 function validate(){
   make -n -f $file_name 2>/dev/stdout
+  if [[ "${file_name}" == "Makefile" ]]; then
+     grep "CDN_BUILD_LIB" "${file_name:?}" || return 0
+     rm -f target/executions/test_makefile
+     make test_makefile
+  fi
 }
