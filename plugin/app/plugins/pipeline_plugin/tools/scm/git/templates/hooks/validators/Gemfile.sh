9000f5bb5aef9362e45d624fe68ebddcd2a9a4fb
carlosg <carlosenrique.gomezgomez@telefonica.com>
2022-05-03 12:22:19 +0200

    Gemfile hook

--- plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/Gemfile.sh
+++ plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/Gemfile.sh
@@ -3,4 +3,7 @@
 function validate(){
   source "$validator_dir"/file_lock_validator.sh
   file_lock_validate "Gemfile.lock"
+  if command -v rubocop  >/dev/null; then
+    rubocop "${file_name:?}"
+  fi
 }
