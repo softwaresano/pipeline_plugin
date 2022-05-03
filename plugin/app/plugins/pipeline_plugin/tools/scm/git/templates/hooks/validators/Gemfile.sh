619d5ae585dd751e8a222453421d78974b1b260a
carlosg <carlosenrique.gomezgomez@telefonica.com>
2022-05-03 13:57:50 +0200

    fix gemfile

--- plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/Gemfile.sh
+++ plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/Gemfile.sh
@@ -2,7 +2,7 @@
 # Gemfile tracked by git and newer than Gemfile.lock
 function validate(){
   source "$validator_dir"/file_lock_validator.sh
-  file_lock_validate "Gemfile.lock"
+  file_lock_validate "Gemfile.lock" || return 1
   if command -v rubocop  >/dev/null; then
     rubocop "${file_name:?}"
   fi
