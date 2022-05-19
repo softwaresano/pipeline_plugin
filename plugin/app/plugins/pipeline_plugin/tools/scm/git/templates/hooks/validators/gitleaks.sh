709ca6f5489ff832d4adbc6e69f3293a0bb52b9f
carlosg <carlosenrique.gomezgomez@telefonica.com>
2022-05-19 19:51:45 +0200

    prepare-commit-msg

--- plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/gitleaks.sh
+++ plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/gitleaks.sh
@@ -3,5 +3,5 @@
 # Validate markdown
 function validate() {
   command -v gitleaks >/dev/null || return 0
-  gitleaks protect --verbose --redact --staged "${file_name:?}"
+  gitleaks protect --verbose --redact --staged
 }
