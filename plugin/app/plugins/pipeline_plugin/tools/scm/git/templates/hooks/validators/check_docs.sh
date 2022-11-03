a10d149efa3f070e804e945746d93c30afbf924d
carlosg <carlosenrique.gomezgomez@telefonica.com>
2022-11-03 08:34:45 +0100

    check_docs script

--- plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/check_docs.sh
+++ plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/check_docs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 function validate() {
-  [[ ! -f .git/tempdir/check_docs ]] && return 0
-  ${CDN_BUILD_LIB:?}/bin/check_docs && rm -f .git/tempdir/check_docs
+  [[ ! -f .git/tempdir/check_docs.sh ]] && return 0
+  .git/tempdir/check_docs.sh  && rm -f .git/tempdir/check_docs
 }
