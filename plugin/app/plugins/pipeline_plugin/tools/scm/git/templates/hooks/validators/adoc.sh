580313bd9e30456e1e19f10521ed074ba3bc6c48
carlosg <carlosenrique.gomezgomez@telefonica.com>
2022-11-03 15:55:11 +0100

    force check_docs

--- plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/adoc.sh
+++ plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/adoc.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-
 # Validate adoc_file
 function validate() {
   [[ ${file_name:?} == *.tpl.adoc ]] && return 126
+  if [[ ! -x .git/tempdir/adoc.sh ]] && grep -q CDN_BUILD_LIB Makefile; then
+    dp_log.sh "[INFO] Run make check_docs to check ${file_name:?}"
+    return 1
+  fi
+
   [[ -x .git/tempdir/adoc.sh ]] || return 126
   .git/tempdir/adoc.sh "${file_name:?}" && touch .git/tempdir/check_docs
 }
+
