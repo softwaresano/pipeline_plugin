abecffa61018abfa1c7199a2708a4397b72bf057
carlosg <carlosenrique.gomezgomez@telefonica.com>
2022-11-19 17:05:33 +0100

    Thirdparty-rpms.txt hook

--- plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/thirdparty-rpms.sh
+++ plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/thirdparty-rpms.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Validate bash
 function validate(){
+  sed -i '/^### Devel packages$/,$d' thirdparty-rpms.txt
+  sed -i '$d' thirdparty-rpms.txt
   grep -q "### Devel packages" "${file_name:?}" && 
     echo "thirdparty-rpms.txt  modified automatically" && return 1
   return 0
