9c17f580bf9cc1a0182167132a8a74f91ba45da4
carlosg <carlosenrique.gomezgomez@telefonica.com>
2022-11-23 07:28:22 +0100

    thirdparty-rpms.txt hook fixed

--- plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/thirdparty-rpms.sh
+++ plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/thirdparty-rpms.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Validate bash
-function validate(){
-  sed -i '/^### Devel packages$/,$d' "${file_name:?}" || return 1
-  sed -i '$d' "${file_name:?}"
+function validate() {
+  if grep -Eq "^### Devel packages$" "${file_name:?}"; then
+    sed -i '/^### Devel packages$/,$d' "${file_name:?}" || return 1
+    sed -i '$d' "${file_name:?}"
+    git add "${file_name:?}"
+  fi
 }
