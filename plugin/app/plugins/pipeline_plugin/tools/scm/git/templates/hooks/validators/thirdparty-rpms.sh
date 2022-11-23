8298bb8f13a44354782f533eab2d5d8b1951edd9
carlosg <carlosenrique.gomezgomez@telefonica.com>
2022-11-23 13:01:17 +0100

    deprecated git add

--- plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/thirdparty-rpms.sh
+++ plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/thirdparty-rpms.sh
@@ -4,6 +4,5 @@ function validate() {
   if grep -Eq "^### Devel packages$" "${file_name:?}"; then
     sed -i '/^### Devel packages$/,$d' "${file_name:?}" || return 1
     sed -i '$d' "${file_name:?}"
-    git add "${file_name:?}"
   fi
 }
