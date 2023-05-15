bc43a62b5642a9834ff96922e8c62dd26bf10389
carlosg <carlosenrique.gomezgomez@telefonica.com>
2023-05-15 18:47:21 +0200

    fix path with whitespaces

--- plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/xml_format.sh
+++ plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/xml_format.sh
@@ -5,7 +5,7 @@ function validate() {
   command -v xmllint >/dev/null || return 126
   local xml_formated_file="${TMPDIR:-/tmp}"/xmllint_format.xml
   rm -f "${xml_formated_file:?}"
-  xmllint --format "$file_name" 2>/dev/stdout > "${xml_formated_file:?}" || return 1
+  xmllint --format "$file_name" 2>/dev/null > "${xml_formated_file:?}" || return 1
   if ! diff "${file_name:?}" "${xml_formated_file:?}"; then
     dp_log.sh "[INFO] Run xmllint --format '${file_name:?}' to fix it"
     return 1
