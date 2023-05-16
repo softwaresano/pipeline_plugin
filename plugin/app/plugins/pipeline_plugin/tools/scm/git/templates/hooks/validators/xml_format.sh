80a8edd7d55cb6bc619971da46a4e68e46acf6b3
carlosg <carlosenrique.gomezgomez@telefonica.com>
2023-05-16 06:41:37 +0200

    fix xml_format

--- plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/xml_format.sh
+++ plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/xml_format.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
+function format_file() {
+  xmllint --format "$file_name" 2>/dev/null  || return 1
+}
+
 # Validate xml_format
 function validate() {
   command -v xmllint >/dev/null || return 126
-  local xml_formated_file="${TMPDIR:-/tmp}"/xmllint_format.xml
-  rm -f "${xml_formated_file:?}"
-  xmllint --format "$file_name" 2>/dev/null > "${xml_formated_file:?}" || return 1
-  if ! diff "${file_name:?}" "${xml_formated_file:?}"; then
-    dp_log.sh "[INFO] Run xmllint --format '${file_name:?}' to fix it"
+  local formated_file
+  formated_file=$(format_file) || return 1
+  if ! diff "${file_name:?}" <(echo "${formated_file:?}"); then
+    dp_log.sh "[INFO] Run ${BASH_SOURCE[0]} '${file_name:?}' to fix it"
     return 1
   fi
+}
+
+function xml_format_fix() {
+  file_name=${1:?}
+  local formated_file
+  formated_file=$(format_file) || return 1
+  echo "${formated_file:?}" > "${file_name:?}"
 }
