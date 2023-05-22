ecfd3b76bac77b94bd68708f795fd79956cb21f8
carlosg <carlosenrique.gomezgomez@telefonica.com>
2023-05-22 11:55:47 +0200

    break hardlinks only with cp -rl

--- plugin/app/plugins/pipeline_plugin/dp_copy_artifacts.sh
+++ plugin/app/plugins/pipeline_plugin/dp_copy_artifacts.sh
@@ -90,9 +90,11 @@ function copy_artifact(){
     rm -rf $target
     mkdir -p $(dirname $target)
     ${DEFAULT_COPY_COMMAND:-cp -rl} $source_file $target
-    copy_repodata "${source_file}" "${target}" 'initiative/noarch'
-    copy_repodata "${source_file}" "${target}" 'initiative/x86_64'
-    copy_repodata "${source_file}" "${target}" '3party'
+    if [[ "${DEFAULT_COPY_COMMAND:-cp -rl}" == "cp -rl" ]]; then
+      copy_repodata "${source_file}" "${target}" 'initiative/noarch'
+      copy_repodata "${source_file}" "${target}" 'initiative/x86_64'
+      copy_repodata "${source_file}" "${target}" '3party'
+    fi
     # Remove extra metadata
     rm -rf $target/.dependencies $target/ur\@* $target/initiative/.noarch $target/initiative/.x86_64
     local error_code=$?
