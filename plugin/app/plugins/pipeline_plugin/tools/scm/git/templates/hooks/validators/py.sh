b6d4e1a45cdbefec911d39b8d9cd474079789cce
carlosg <carlosenrique.gomezgomez@telefonica.com>
2023-10-25 12:09:50 +0200

    py3.9 validator

--- plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/py.sh
+++ plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/py.sh
@@ -2,5 +2,5 @@
 
 # Validate python_file
 function validate() {
-  python3 -c "import py_compile; py_compile.compile('$file_name', doraise='True')"
+  python3.9 -c "import py_compile; py_compile.compile('$file_name', doraise='True')"
 }
