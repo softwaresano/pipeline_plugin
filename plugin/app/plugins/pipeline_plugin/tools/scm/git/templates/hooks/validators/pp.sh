2822082ca5382156aa0b947339cb627bc6fb4eef
caenco <carlos.encabocorrales@telefonica.com>
2023-10-26 13:03:42 +0200

    no legacy facts check

--- plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/pp.sh
+++ plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/pp.sh
@@ -6,8 +6,8 @@ function validate() {
   #shellcheck source=./plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/pre-commit
   /opt/puppetlabs/bin/puppet parser validate "$file_name" 2>/dev/stdout || return 1
   command -v puppet-lint >/dev/null || return 126
-  if puppet-lint --no-autoloader_layout --no-puppet_url_without_modules --no-80chars --no-top_scope_facts --with-filename "$file_name"|grep "\- WARNING: "; then
-    dp_log.sh "[INFO] puppet-lint --no-autoloader_layout --no-puppet_url_without_modules --no-80chars --no-top_scope_facts --fix --with-filename "$file_name""
+  if puppet-lint --no-autoloader_layout --no-puppet_url_without_modules --no-80chars --no-top_scope_facts --no-legacy_facts --with-filename "$file_name"|grep "\- WARNING: "; then
+    dp_log.sh "[INFO] puppet-lint --no-autoloader_layout --no-puppet_url_without_modules --no-80chars --no-top_scope_facts --no-legacy_facts --fix --with-filename "$file_name""
   return 1
   fi
 }
