0190e3106abd2c60f8c15162d5ca318467144604
carlosg <carlosenrique.gomezgomez@telefonica.com>
2023-11-09 08:22:10 +0100

    warnings fixed

--- plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/prettier.sh
+++ plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/prettier.sh
@@ -4,7 +4,7 @@
 function validate() {
   [[ ${file_name:?} == *.min.js ]] && return 0
   command -v prettier &>/dev/null || return 126
-  if prettier --no-color --check "${file_name:?}"|grep -q '^\[warn] '; then
+  if prettier --no-color --check "${file_name:?}" |& grep -q '^\[warn] '; then
     prettier "${file_name:?}"
     dp_log.sh "[INFO] prettier --write ${file_name:?} to fix it"
     return 1
