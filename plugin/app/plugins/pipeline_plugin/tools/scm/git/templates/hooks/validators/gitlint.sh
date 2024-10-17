a62598ed1844667fc3d008ff52eba41e06affde3
carlosegg <carlosenrique.gomezgomez@telefonica.com>
2024-10-17 11:00:17 +0200

    codespell in commit message

--- plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/gitlint.sh
+++ plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/gitlint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 function validate() {
+  if command -v codespell &>/dev/null; then
+    echo "${commit_message}"|codespell - || return 1	
+  fi
   echo "$commit_message" | PYTHONUSERBASE=/opt/p2pcdn/var/lib/gitlint/ \
     /opt/p2pcdn/var/lib/gitlint/bin/gitlint \
     --config "${CDN_BUILD_LIB:-/opt/p2pcdn/var/lib/build}"/linters/gitlint.ini
