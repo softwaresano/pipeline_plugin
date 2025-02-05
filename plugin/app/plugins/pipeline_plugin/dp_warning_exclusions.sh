6e42a5c73ea9a3b47a78c2e2f24004da8ab6f294
carlosegg <carlosenrique.gomezgomez@telefonica.com>
2025-02-05 21:07:18 +0100

    nosec exclusion

--- plugin/app/plugins/pipeline_plugin/dp_warning_exclusions.sh
+++ plugin/app/plugins/pipeline_plugin/dp_warning_exclusions.sh
@@ -2,5 +2,5 @@
 reports_path=${REPORTS_PATH:-target/reports}
 only_repo=($(git ls-tree --name-only HEAD))
 mkdir -p "${reports_path:?}"
-grep -Ern '^BANDIT_EXCLUSIONS=|^CODESPELL_EXCLUSIONS=|^NO_TESTING_RESOURCES_REGEX=|/\*( |\t)*cppcheck-suppress|//( |\t)*cppcheck-suppress|^EXTRA_JSCPD_IGNORE=|#( |\t)*pylint:( |\t)*skip-file|#( |\t)*pragma( |\t)no( |\t)cover|#( |\t)*shellcheck( |\t)*disable=( |\t)*|#( |\t)*rubocop:( |\t)*disable|#( |\t)*pylint:( |\t)*disable|#( |\t)*NOSONAR|sonar\.exclusions( |\t)*=|#( |\t)*lint:ignore|( |\t)*/\*( |\t)*NOSONAR( |\t)*\*/|//( |\t)*NOSONAR|( |\t)*/\*( |\t)*eslint-disable( |\t)*\*/|//( |\t)*eslint-disable' "${only_repo[@]}" | grep -Ev "^external/" >"${reports_path:?}/warning-exclusions.txt"
+grep -Ern '#( |\t)*nosec( |\t)*|^BANDIT_EXCLUSIONS=|^CODESPELL_EXCLUSIONS=|^NO_TESTING_RESOURCES_REGEX=|/\*( |\t)*cppcheck-suppress|//( |\t)*cppcheck-suppress|^EXTRA_JSCPD_IGNORE=|#( |\t)*pylint:( |\t)*skip-file|#( |\t)*pragma( |\t)no( |\t)cover|#( |\t)*shellcheck( |\t)*disable=( |\t)*|#( |\t)*rubocop:( |\t)*disable|#( |\t)*pylint:( |\t)*disable|#( |\t)*NOSONAR|sonar\.exclusions( |\t)*=|#( |\t)*lint:ignore|( |\t)*/\*( |\t)*NOSONAR( |\t)*\*/|//( |\t)*NOSONAR|( |\t)*/\*( |\t)*eslint-disable( |\t)*\*/|//( |\t)*eslint-disable' "${only_repo[@]}" | grep -Ev "^external/" >"${reports_path:?}/warning-exclusions.txt"
 exit 0
