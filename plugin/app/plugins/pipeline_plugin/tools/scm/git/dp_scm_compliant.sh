c28a75d761b42c0d96b39d8e7b657e06f128a149
carlosegg <carlosenrique.gomezgomez@telefonica.com>
2025-03-11 11:04:24 +0100

    jira_prefix

--- plugin/app/plugins/pipeline_plugin/tools/scm/git/dp_scm_compliant.sh
+++ plugin/app/plugins/pipeline_plugin/tools/scm/git/dp_scm_compliant.sh
@@ -5,30 +5,19 @@ function get_repo_project_name(){
   git tag |grep -Po '.*(?=/Project$)'
 }
 
-function get_git_prefix_message(){
-  project_name=$1
-  jira_prefix=$2
-  repo_project_name=$(get_repo_project_name)
-  [[ "$repo_project_name" != "$project_name" ]] && return 0
-  jira_prefix_id="$jira_prefix-[0-9]+_"
-  branch_stability_type=$(dp_branch_type.sh)
-  if [[ "$branch_stability_type" == "unstable" || "$branch_stability_type" == "other" ]]; then
-    IFS="/"
-    set $current_branch
-    unset IFS
-    branch_type=$1
-    branch_name=$2
-    branch_types="feature|bug|hotfix|task"
-    [[ "${current_branch}" =~ ^(${branch_types})/${jira_prefix}-[0-9]+_ ]] && echo "$branch_name"|sed 's:_.*::g' && return 0
-    [[ "$current_branch" == 'HEAD' ]] && [[ "${JOB_URL}" != '' ]] && return 0
-    [[ "$jira_prefix" == "WITHOUT_JIRA" ]] && [[ "$branch_stability_type" == "unstable" ]] && echo "" && return 0
-    dp_log.sh "[ERROR] [$current_branch] is not a valid $project_name branch name."
-    [[ "$jira_prefix" != "WITHOUT_JIRA" ]] && \
-        dp_log.sh "[ERROR] The valid branch name is: <$branch_types>/$jira_prefix-XXXX_$branch_name, where XXXX is the jira-id associated with a jira task" || \
-        dp_log.sh "[ERROR] The valid branch name is: <$branch_types>/$([[ "$branch_name" == "" ]] && echo $current_branch || echo $branch_name)"
+function get_git_prefix_message() {
+  local project_name=${1:?}
+  local current_branch=${2:?}
+  local jira_prefix=${3:?}
+
+  [[ ${current_branch:?} =~ ${jira_prefix:?} ]] && return 0
+  dp_log.sh "[ERROR] [$current_branch] is not a valid $project_name branch name."
+  if [[ "$jira_prefix" != "WITHOUT_JIRA" ]]; then
+    dp_log.sh "[ERROR] The valid branch name is: ${jira_prefix:?}"
     return 1
   fi
 }
+
 project_name=$(get_repo_project_name)
 projects_id_dir=$(git rev-parse --show-toplevel)/.git/hooks/projects_id
 jira_prefix_file=$projects_id_dir/$project_name/jira_prefix
@@ -36,7 +25,7 @@ jira_prefix=$(cat $jira_prefix_file 2>/dev/null)
 
 if [[ "$jira_prefix" == "" ]]; then
   if [[ "$project_name" == "cdn" ]]; then
-    jira_prefix="(PTWOPCDN|PTWOPCDNTC|TCDNOPTSCB|GMETRICS|OPSSUP|VTOOLS)"
+    jira_prefix='^(develop|master|release/[0-9]{2}\.[0-9]{1,2}\.[0-9]{3}|(feature|task|hotfix|bug)/(PTWOPCDN|PTWOPCDNTC|TCDNOPTSCB|GMETRICS|OPSSUP|VTOOLS)-[0-9]+_.+)$'
   else
     dp_log.sh "[ERROR] There aren't any jira_prefix for $project_name organization"
     dp_log.sh "[ERROR] Add jira-prefix (Ex: PTWOPCDN) in $jira_prefix_file file or assigns WITHOUT_JIRA to disable jira_prefix"
@@ -46,4 +35,4 @@ fi
 
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 [[ "${current_branch}" =~ ^dependabot ]] && return 0
-get_git_prefix_message "$project_name" "$jira_prefix"
+get_git_prefix_message "${project_name:?}" "${current_branch:?}" "$jira_prefix"
