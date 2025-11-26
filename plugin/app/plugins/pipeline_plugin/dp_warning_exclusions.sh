0ceae1b2f2daa48266631d5350944e046ab9572e
leete_la_documentacion <leete_la_documentacion.melon@telefonica.com>
2025-11-26 13:14:54 +0100

    Detect CHECK_COVERAGE_PYTHON

--- plugin/app/plugins/pipeline_plugin/dp_warning_exclusions.sh
+++ plugin/app/plugins/pipeline_plugin/dp_warning_exclusions.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 reports_path=${REPORTS_PATH:-target/reports}
-only_repo=($(git ls-tree --name-only HEAD))
+only_repo=($(git ls-tree --name-only HEAD| grep -v '^Makefile$'))
 mkdir -p "${reports_path:?}"
-grep -Ern '#( |\t)*nosec( |\t)*|^YAML_ERRORS_PATH=|^BANDIT_EXCLUSIONS=|^CODESPELL_EXCLUSIONS=|^NO_TESTING_RESOURCES_REGEX=|/\*( |\t)*cppcheck-suppress|//( |\t)*cppcheck-suppress|^EXTRA_JSCPD_IGNORE=|#( |\t)*type:( |\t)* ignore|#( |\t)*pylint:( |\t)*skip-file|#( |\t)*pragma( |\t)no( |\t)cover|#( |\t)*shellcheck( |\t)*disable=( |\t)*|#( |\t)*rubocop:( |\t)*disable|#( |\t)*pylint:( |\t)*disable|#( |\t)*NOSONAR|sonar\.exclusions( |\t)*=|#( |\t)*lint:ignore|( |\t)*/\*( |\t)*NOSONAR( |\t)*\*/|//( |\t)*NOSONAR|( |\t)*/\*( |\t)*eslint-disable( |\t)*\*/|//( |\t)*eslint-disable' "${only_repo[@]}" | grep -Ev "^external/" >"${reports_path:?}/warning-exclusions.txt"
+grep -Ern '#( |\t)*nosec( |\t)*|/\*( |\t)*cppcheck-suppress|//( |\t)*cppcheck-suppress|^EXTRA_JSCPD_IGNORE=|#( |\t)*type:( |\t)* ignore|#( |\t)*pylint:( |\t)*skip-file|#( |\t)*pragma( |\t)no( |\t)cover|#( |\t)*shellcheck( |\t)*disable=( |\t)*|#( |\t)*rubocop:( |\t)*disable|#( |\t)*pylint:( |\t)*disable|#( |\t)*NOSONAR|sonar\.exclusions( |\t)*=|#( |\t)*lint:ignore|( |\t)*/\*( |\t)*NOSONAR( |\t)*\*/|//( |\t)*NOSONAR|( |\t)*/\*( |\t)*eslint-disable( |\t)*\*/|//( |\t)*eslint-disable' "${only_repo[@]}" | grep -Ev "^external/" >"${reports_path:?}/warning-exclusions.txt"
+if git ls-files | grep -xq Makefile; then
+  grep -Ern '^YAML_ERRORS_PATH=|^BANDIT_EXCLUSIONS=|^CODESPELL_EXCLUSIONS=|^NO_TESTING_RESOURCES_REGEX=|^CHECK_COVERAGE_PYTHON=' * |grep '^Makefile:'>>"${reports_path:?}/warning-exclusions.txt"
+fi
 exit 0
