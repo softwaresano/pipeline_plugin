a08bafbf431d5beaa17a7c008c6455ffba150b7b
carlosegg <carlosenrique.gomezgomez@telefonica.com>
2026-02-12 09:09:32 +0100

    message format

--- plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/copyscan.sh
+++ plugin/app/plugins/pipeline_plugin/tools/scm/git/templates/hooks/validators/copyscan.sh
@@ -10,8 +10,9 @@ function validate() {
   if is_cdn_build; then
     source "${CDN_BUILD_LIB:?}"/bin/copy_scan.sh
     message=$(SCM_FILES_CMD=copyscan run_copy_scan)
-    if [[ ${message} != '' ]]; then
-      dp_log.sh "[ERROR] The file $file_name has a copyright. You can not commit a file with copyright: ${message:?}"
+    if [[ "${message}" != '' ]]; then
+      #dp_log.sh "[ERROR] The file $file_name has a copyright. You can not commit a file with copyright: ${message:?}"
+      dp_log.sh "[ERROR] ${message:?}"
       return 1
     fi
   fi
